e014a364e73ec9667d16b3fba70f7dd5e517ed5c
Kuni <kuni.ujiie@grapecity.com>
2022-10-19 11:39:17 +0900

    プロジェクト ファイルを追加します。

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiApp1.ViewModels;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        BindingContext = new MonkeysViewModel();
+    }
+
+}
+
